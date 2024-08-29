134ae3fe4cdf9db390cc13f3a82202a38c31a389
truuue <noahvernhet@gmail.com>
2024-08-29 16:50:58 +0200

    Remove useless console.log

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./components/app/App";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,4 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals(console.log);
+reportWebVitals();
