ceddbb1e2de5ba958b78c05f4fdf671a8cce3623
truuue <noahvernhet@gmail.com>
2024-08-31 23:57:00 +0200

    Ident the component HourlyForecast and set the width to 100%

--- src/components/weather/HourlyForecast.tsx
+++ src/components/weather/HourlyForecast.tsx
@@ -9,11 +9,14 @@ const formatTime = (date: Date): string => {
 
 const HourlyForecast: React.FC<{ data: any[] }> = ({ data }) => {
   return (
-    <div className="bg-gradient-to-r from-blue-400 to-blue-600 rounded-xl shadow-lg p-4 max-w-md mx-auto">
+    <div className="bg-gradient-to-r from-blue-400 to-blue-600 rounded-xl shadow-lg p-4 max-w-md w-full mx-auto">
       <h3 className="text-white text-lg font-semibold mb-3">Hourly Forecast</h3>
-      <div className="flex justify-between ">
+      <div className="flex justify-between">
         {data.map((item, index) => (
-          <div key={index} className="text-center bg-white bg-opacity-20 rounded-lg p-2">
+          <div
+            key={index}
+            className="text-center bg-white bg-opacity-20 rounded-lg p-2"
+          >
             <p className="text-white font-medium">{item.time}</p>
             <img
               src={`http://openweathermap.org/img/w/${item.icon}.png`}
