39b14db2f76b52e072d73ebd0a4d191dbc97691e
truuue <noahvernhet@gmail.com>
2024-08-31 20:20:22 +0200

    Improve structure and styling of HourlyForecast component

--- src/components/weather/HourlyForecast.tsx
+++ src/components/weather/HourlyForecast.tsx
@@ -11,9 +11,9 @@ const HourlyForecast: React.FC<{ data: any[] }> = ({ data }) => {
   return (
     <div className="bg-gradient-to-r from-blue-400 to-blue-600 rounded-xl shadow-lg p-4 max-w-md mx-auto">
       <h3 className="text-white text-lg font-semibold mb-3">Hourly Forecast</h3>
-      <div className="flex justify-between">
+      <div className="flex justify-between ">
         {data.map((item, index) => (
-          <div key={index} className="text-center">
+          <div key={index} className="text-center bg-white bg-opacity-20 rounded-lg p-2">
             <p className="text-white font-medium">{item.time}</p>
             <img
               src={`http://openweathermap.org/img/w/${item.icon}.png`}
