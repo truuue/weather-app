1776d3f4a618e05c9b62424608b60d61745ace01
truuue <noahvernhet@gmail.com>
2024-08-31 23:55:57 +0200

    Remove margin bottom on Locale component

--- src/components/body/Locale.tsx
+++ src/components/body/Locale.tsx
@@ -6,7 +6,7 @@ interface LocaleProps {
 
 const Locale: React.FC<LocaleProps> = ({ city }) => {
   return (
-    <div className="flex flex-col justify-center items-center p-4 bg-gradient-to-b from-blue-400 to-blue-600 rounded-xl shadow-lg text-white mb-4">
+    <div className="flex flex-col justify-center items-center p-4 bg-gradient-to-b from-blue-400 to-blue-600 rounded-xl shadow-lg text-white">
       <h2 className="text-xl font-semibold mb-2">{city}</h2>
       <p className="text-2xl font-semibold">
         {new Date().toLocaleTimeString([], {
