0c23671845d8a05202998959b9b5fbb7a42f9b3f
truuue <noahvernhet@gmail.com>
2024-08-31 20:00:53 +0200

    Refactor Body component for improved structure and styling

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -14,33 +14,53 @@ const Body: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen">
-      <h1 className="text-4xl font-semibold pb-5">Weather App</h1>
-      <p className="text-lg pb-5">
-        This is a weather app that allows you to check the weather in different
-        cities.
-      </p>
+    <div className="flex flex-col justify-start items-center w-screen min-h-screen p-4 overflow-y-auto">
+      {/* hidden title for referencing */}
+      <h1 className="hidden">Weather App</h1>
 
+      {/* Hour and city */}
+      <div className="flex flex-col justify-center items-center p-4 bg-gray-100 rounded-lg mb-4">
+        <h2 className="text-xl font-semibold mb-2">{city}</h2>
+        <p className="text-2xl font-semibold">
+          {new Date().toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          })}
+        </p>
+        <p className="text-sm">
+          {new Date().toLocaleDateString("en-EN", {
+            weekday: "long",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
+      </div>
+
+      {/* Search bar */}
       <form
         onSubmit={handleSubmit}
-        className="flex flex-row justify-center items-center gap-2 pb-5"
+        className="flex flex-row justify-center items-center gap-2 mb-4"
       >
         <input
           type="text"
           value={inputCity}
           onChange={(e) => setInputCity(e.target.value)}
           placeholder="Enter city name"
-          className="p-1 border border-gray-200 rounded-lg shadow"
+          className="p-2 border border-gray-200 rounded-lg shadow"
         />
         <button
           type="submit"
-          className="p-1 border border-gray-200 rounded-lg shadow"
+          className="p-2 border border-gray-200 rounded-lg shadow"
         >
           Search
         </button>
       </form>
 
-      <Weather city={city} />
+      {/* Weather */}
+      <div className="w-full max-w-md">
+        <Weather city={city} />
+      </div>
     </div>
   );
 };
